ca43ec47a0199d9dda61ce643bd9620dbee0b51e
detarso7 <38414640+detarso7@users.noreply.github.com>
2018-09-21 17:17:18 -0300

    Recuperando o ID Token do current user

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,9 @@ import { Usuario } from "./acesso/usuario.model";
 import * as fb from "firebase"
 
 export class Auth{
+
+    public token_id: string
+
     public cadastroDeUsuario(usuario:Usuario):Promise<any>{
 
        return fb.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
@@ -21,7 +24,12 @@ export class Auth{
 
     public autenticar(email:string, senha:string):void{
         fb.auth().signInWithEmailAndPassword(email, senha)
-        .then((resposta: any) => console.log(resposta))
+        .then((resposta: any) =>{
+            fb.auth().currentUser.getIdToken()
+            .then((inToken)=>{
+                this.token_id = inToken
+            })
+        })
         .catch((err:any)=> console.log(err))
     }
 }
